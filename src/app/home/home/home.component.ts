24a11474a209baba98a00f44418180802c96ee42
Hugo-Chea <hugo.chea@diggers-consulting.com>
2023-06-29 12:27:56 +0200

    init example code

--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
 
+export interface TodoItem {
+  name: string,
+  checked: boolean,
+}
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+
+  input: string= '';
+
+  todoList: TodoItem[] = [
+    {
+      name: 'Hit the gym',
+      checked: false
+    },
+    {
+      name: 'Pay bills',
+      checked: true
+    },
+    {
+      name: 'Meet George',
+      checked: true
+    }
+  ];
 
 }
